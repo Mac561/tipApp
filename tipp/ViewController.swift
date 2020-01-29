602a6c313675a2befa5571d4ac4e971b1f847f0d
Mac561 <maccooper561@gmail.com>
2020-01-28 17:38:29 -0800

    Initial Commit

---
+++ tipp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipp
+//
+//  Created by Mac Cooper on 1/28/20.
+//  Copyright © 2020 Mac Cooper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
